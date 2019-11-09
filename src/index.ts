600a79a87eee9a7e2927c1b9b6b191e13c2a213b
adamjoha <adamtorkkeli@gmail.com>
2019-11-10 00:36:51 +0100

    update with neo4j connection and example query

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+const neo4j = require('neo4j-driver').v1;
+
+const driver = neo4j.driver("bolt://localhost:11002", neo4j.auth.basic(process.env.NEO4J_USERNAME, process.env.NEO4J_PASSWORD));
+const session = driver.session();
+
+const personName = 'Alice';
+const resultPromise = session.run(
+  'MATCH (n: Exercise) return n',
+  {name: personName}
+);
+
+resultPromise.then((result: any) => {
+  session.close();
+  //here you can do something with the result.
+
+  // on application exit:
+  driver.close();
+});
