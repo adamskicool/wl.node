9c732873d4c2bf7539965a4ac5a9c9687f88cfac
adamjoha <adamtorkkeli@gmail.com>
2020-01-05 17:17:26 +0100

    add tslint and prettier to project

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
-require('dotenv').config()
-import "reflect-metadata";
-import {createConnection} from "typeorm";
-import express = require('express')
+require('dotenv').config();
+import 'reflect-metadata';
+import {createConnection} from 'typeorm';
+import express = require('express');
 
-createConnection().then(async connection => {
-    const app = express()
-    
-    app.get("/hello", (req, res) => {
-        res.send("Hello world");
-    });
-    
-    app.listen(process.env.API_PORT, () => {
-        console.log(`Server is listening on port: ${process.env.API_PORT}`);
-    });
-}).catch(error => console.log(error));
+createConnection()
+	.then(async (connection) => {
+		const app = express();
+
+		app.get('/hello', (req, res) => {
+			res.send('Hello world');
+		});
+
+		app.listen(process.env.API_PORT, () => {
+			console.log(`Server is listening on port: ${process.env.API_PORT}`);
+		});
+	})
+	.catch((error) => console.log(error));
