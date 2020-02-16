287540f05ebad01277f81e2dcefa3d3b9f355a5d
Adam Torkkeli <adam.torkkeli@getaccept.com>
2020-02-16 16:53:58 +0100

    check for the JWT token

--- src/api/route.ts
+++ src/api/route.ts
@@ -1,5 +1,15 @@
+import { verifyJWTToken } from "./middleware";
+
 module.exports = app => {
-  app.use("/api/exercise", require("./controllers/exercise/router"));
-  app.use("/api/muscleArea", require("./controllers/muscle-area/router"));
+  app.use(
+    "/api/exercise",
+    verifyJWTToken,
+    require("./controllers/exercise/router")
+  );
+  app.use(
+    "/api/muscleArea",
+    verifyJWTToken,
+    require("./controllers/muscle-area/router")
+  );
   app.use("/api/user", require("./controllers/user/router"));
 };
