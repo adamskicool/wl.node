0045a06bb5cc37ed7dace7045c8ec538a09630e5
Adam Torkkeli <adamtorkkeli@gmail.com>
2020-07-16 13:12:46 +0200

    update (#20)

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,74 +1,67 @@
-import {
-  BaseEntity,
-  Column,
-  Entity,
-  Index,
-  JoinColumn,
-  JoinTable,
-  ManyToMany,
-  ManyToOne,
-  OneToMany,
-  OneToOne,
-  PrimaryColumn,
-  PrimaryGeneratedColumn,
-  RelationId
-} from "typeorm";
-import { Workout } from "./Workout";
-import { Exercise } from "./Exercise";
+import {Column, Entity, OneToMany} from 'typeorm';
+import {Workout} from './Workout';
+import {Exercise} from './Exercise';
 
-@Entity("User", { schema: "workout_logger" })
+@Entity('User', {schema: 'workout_logger'})
 export class User {
-  @Column("varchar", {
-    nullable: false,
-    primary: true,
-    length: 36,
-    name: "id"
-  })
-  id: string;
+	@Column('varchar', {
+		nullable: false,
+		primary: true,
+		length: 36,
+		name: 'id'
+	})
+	id: string;
 
-  @Column("varchar", {
-    nullable: false,
-    length: 100,
-    name: "username",
-    unique: false
-  })
-  username: string;
+	@Column('varchar', {
+		nullable: false,
+		length: 100,
+		name: 'username',
+		unique: false
+	})
+	username: string;
 
-  @Column("varchar", {
-    nullable: false,
-    length: 100,
-    name: "password"
-  })
-  password: string;
+	@Column('varchar', {
+		nullable: false,
+		length: 100,
+		name: 'password'
+	})
+	password: string;
 
-  @Column("varchar", {
-    nullable: false,
-    length: 100,
-    name: "firstName"
-  })
-  firstName: string;
+	@Column('varchar', {
+		nullable: false,
+		length: 100,
+		name: 'firstName'
+	})
+	firstName: string;
 
-  @Column("varchar", {
-    nullable: false,
-    length: 100,
-    name: "lastName"
-  })
-  lastName: string;
+	@Column('varchar', {
+		nullable: false,
+		length: 100,
+		name: 'lastName'
+	})
+	lastName: string;
 
-  @Column("text", {
-    nullable: true,
-    name: "googleId"
-  })
-  googleId: string | null;
+	@Column('varchar', {
+		nullable: false,
+		length: 100,
+		name: 'email'
+	})
+	email: string;
 
-  @OneToMany(
-    () => Workout,
-    (Workout: Workout) => Workout.user,
-    { onDelete: "NO ACTION", onUpdate: "NO ACTION" }
-  )
-  workouts: Workout[];
+	@Column('text', {
+		nullable: true,
+		name: 'googleId'
+	})
+	googleId: string | null;
 
-  @OneToMany(
+	@OneToMany(
+		() => Workout,
+		(Workout: Workout) => Workout.user,
+		{onDelete: 'NO ACTION', onUpdate: 'NO ACTION'}
+	)
+	workouts: Workout[];
+
+	@OneToMany(
 		() => Exercise,
 		(Exercise: Exercise) => Exercise.user,
 		{onDelete: 'NO ACTION', onUpdate: 'NO ACTION'}
