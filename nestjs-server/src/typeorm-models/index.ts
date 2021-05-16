525229c13f88c5fa080d173442ee7c1156f5eebe
adamjoha <adamtorkkeli@gmail.com>
2021-05-16 19:34:30 +0200

    remove typeorm modules

--- nestjs-server/src/typeorm-models/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './Exercise';
-export * from './MuscleArea';
-export * from './PresetExerciseSet';
-export * from './User';
-export * from './Workout';
-export * from './WorkoutPreset';
-export * from './WorkoutSet';
